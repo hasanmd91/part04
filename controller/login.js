dbc06eca15a7f1db4394691cb324584e6cfd3026
hasan md <hasan.md@quanturi.com>
2023-07-09 01:20:39 +0300

    added loginRouter

---
+++ controller/login.js
@@ -0,0 +1,29 @@
+const loginRouter = require('express').Router();
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+loginRouter.post('/', async (response, request) => {
+  const { username, password } = request.body;
+
+  const user = await User.findOne({ username });
+
+  const passwordCorrect =
+    user === null ? false : await bcrypt.compare(password, user.passwordHash);
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password',
+    });
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user.id,
+  };
+
+  const token = jwt.sign(userForToken, process.env.SECRET);
+  response.status(200).send({ token, username: user.username, id: user.id });
+});
+
+module.exports = loginRouter;
