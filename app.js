847d21717d91a6a94d1de4733bf8834dd8ef4163
hasan md <hasan.md@quanturi.com>
2023-08-08 16:22:13 +0300

    added testing reset router

--- app.js
+++ app.js
@@ -31,6 +31,11 @@ app.use('/api/blogs', userExtractor, blogRouter);
 app.use('/api/users', userRouter);
 app.use('/api/login', loginRouter);
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controller/testing.js');
+  app.use('/api/testing', testingRouter);
+}
+
 app.use(unknownEndpoint);
 app.use(errorHandeler);
 
